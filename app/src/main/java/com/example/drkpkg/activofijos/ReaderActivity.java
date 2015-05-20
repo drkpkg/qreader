a5f8f663d6d218bcc4aa1a118b4a3b40af704d0d
Felix Coca <daniel.uremix@gmail.com>
2015-05-20 17:29:40 -0400

    Modificada vistas.

--- app/src/main/java/com/example/drkpkg/activofijos/ReaderActivity.java
+++ app/src/main/java/com/example/drkpkg/activofijos/ReaderActivity.java
@@ -2,40 +2,15 @@ package com.example.drkpkg.activofijos;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
 
 public class ReaderActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reader);
         new IntentIntegrator(this).initiateScan();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_reader, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
